8e9faa7ae45eaaff4a80d42808711d74ceab517e
lucasbc92 <lbc92@hotmail.com>
2020-07-26 18:08:54 -0300

    Get and put data in sheet

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,6 +1,32 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import credentials from '../../credentials.json'
+
+const doc = new GoogleSpreadsheet('1RuEjl4QOVb-AW9VKr-kHYMiAlp5sgmeiTzzrxk_rYFc');
+
 export default async(req, res) => {
-    res.end(JSON.stringify({
-        showCoupon: true,
-        message: 'Coupon message'
-    }))
+    try {
+        await doc.useServiceAccountAuth(credentials);
+        await doc.loadInfo();
+    
+        const sheet = doc.sheetsByIndex[2];
+    
+        await sheet.loadCells('A3:B3');
+        console.log(sheet.title);
+    
+        const showCoupon = sheet.getCell(2,0).value; 
+        console.log(showCoupon);
+
+        const message = sheet.getCell(2,1).value;
+        console.log(message);
+
+        res.end(JSON.stringify({
+            showCoupon,
+            message
+        }))
+    } catch (err) {
+        res.end(JSON.stringify({
+            showCoupon: false,
+            message: ''
+        }))
+    }
 }
--- pages/api/save.js
+++ pages/api/save.js
@@ -1,4 +1,29 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import credentials from '../../credentials.json'
+
+const doc = new GoogleSpreadsheet('1RuEjl4QOVb-AW9VKr-kHYMiAlp5sgmeiTzzrxk_rYFc');
+
 export default async(req, res) => {
-    console.log(JSON.parse(req.body))
-    res.end(req.body)
+    try {
+        await doc.useServiceAccountAuth(credentials);
+        await doc.loadInfo();
+    
+        const { Name, Email, Whatsapp } = JSON.parse(req.body);
+        
+        const sheet = doc.sheetsByIndex[1];
+        // Name	Email	Whatsapp	Coupon	Promo
+    
+        await sheet.addRow({
+            Name,
+            Email,
+            Whatsapp,
+            Coupon: 'aaa111',
+            Promo: 'gyaygsygas'
+        })
+    
+        res.end(req.body)
+    } catch (err) {
+        res.end('error');
+    }
+
 }
